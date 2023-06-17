ddabbea8b5fe6ecb8df42f0b5fc98ac6afd445b2
Amaan1787 <sayedamaan03@gmail.com>
2023-06-17 00:11:27 -0400

    intial commit

---
+++ src/components/playlist/Playlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function playlist() {
+  return (
+    <div>
+      <h1>Playlist page</h1>
+    </div>
+  )
+}
+
+export default playlist
---
+++ src/components/track/Track.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function track() {
+  return (
+    <div>
+      <h1>Track page</h1>
+    </div>
+  )
+}
+
+export default track
